c2314bef5803e8c482e3c5714b12eea7c9733635
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-19 21:42:46 +0330

    Display favCount and totalCount in the template
    Updated the template to show the number of favorite tasks (favCount) and total tasks (totalCount) using taskStore getters. This helps the user see how many tasks are marked as favorite and how many are left to do.

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@
 
     <!--task list-->
     <div class="task-list" v-if="filter === 'all'">
-      <p>All tasks</p>
+      <p>You have {{ taskStore.totalCount }} tasks left to do.</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>Favorite tasks</p>
+      <p>You have {{ taskStore.favCount }} tasks in your favs list.</p>
       <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
