5f0035f889373f2e407ba54cc0825aa81f56de89
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-19 22:02:07 +0330

    Add a form component to submit the new tasks

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="I need to..." v-model="newTask" />
+    <button>Add</button>
+  </form>
+</template>
 
 <script>
 export default {};
