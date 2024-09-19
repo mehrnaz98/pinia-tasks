64c5ee4937b973acc518fd9b3259d16fca2baf03
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-19 16:02:11 +0330

    Add two icons, one to delete the tasks and one to mark them as favorite

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="task">
     <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
   </div>
 </template>
 
