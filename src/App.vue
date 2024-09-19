437004c650d6338bb54c4ac9a9a4208c954968b7
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-19 15:47:47 +0330

    Cycle through the tasks and output the titles

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia tasks</h1>
     </header>
+
+    <!--task list-->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        {{ task.title }}
+      </div>
+    </div>
   </main>
 </template>
 
