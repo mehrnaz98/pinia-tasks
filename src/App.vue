341a743081b87102d00346b21421eb709e54f318
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-19 22:31:18 +0330

    Add taskForm component into the App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
       <h1>Pinia tasks</h1>
     </header>
 
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
     <!-- filter -->
     <nav class="filter">
       <button @click="filter = 'all'">All tasks</button>
