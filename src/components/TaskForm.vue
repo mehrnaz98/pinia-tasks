d6ddb0ef9544833d5cba9fe8a85ce928ada29086
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-21 19:20:47 +0330

    Update TaskForm.vue

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <input type="text" placeholder="I need to..." v-model="newTask" />
-    <button>Add</button>
+    <button @keyup="handleSubmit">Add</button>
   </form>
 </template>
 
